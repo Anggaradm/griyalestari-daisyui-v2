d726a09fda46ca994aca1aa519d7bbf00f1edd74
Angga rizki <82260409+Anggaradm@users.noreply.github.com>
2023-07-15 11:38:25 +0700

    repair

--- src/components/molecules/signforms/SigninForm.jsx
+++ src/components/molecules/signforms/SigninForm.jsx
@@ -1,3 +1,4 @@
+import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
@@ -26,9 +27,22 @@ const SigninForm = () => {
   // consumeAPI
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const { user, isError, isSuccess, isLoading, message } = useSelector(
+  const { isError, isSuccess, isLoading, message } = useSelector(
     (state) => state.auth
   );
+
+  const serverUrl = process.env.REACT_APP_SERVER_URL;
+  const [user, setUser] = useState({});
+  const getUser = async () => {
+    await axios.get(`${serverUrl}/auth`).then((response) => {
+      const data = response.data;
+      setUser(data);
+    });
+  };
+
+  useEffect(() => {
+    getUser();
+  }, [user]);
 
   useEffect(() => {
     if (user || isSuccess) {
